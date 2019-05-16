a985d3407aa71f30cf86696ee0a2f409709f22e1
Koichi Shiraishi <zchee.io@gmail.com>
2019-05-17 02:31:01 +0900

    xerrors: Revert "xerrors: redirect to go1.13 primitives for 1.13"
    This reverts CL 167577
    
    Reason for revert:
    errors.Frame and errors.Formatter were removed in CL 176997
    
    Fixes golang/go#32246
    
    Change-Id: I521cc1176311721a18aaf76d4966e945c8453e9d
    Reviewed-on: https://go-review.googlesource.com/c/xerrors/+/177379
    Run-TryBot: Agniva De Sarker <agniva.quicksilver@gmail.com>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Matt Joiner <anacrolix@gmail.com>
    Reviewed-by: Damien Neil <dneil@google.com>

--- fmt_test.go
+++ fmt_test.go
@@ -7,6 +7,7 @@ package xerrors_test
 import (
 	"fmt"
 	"io"
+	"os"
 	"path"
 	"reflect"
 	"regexp"
@@ -90,7 +91,7 @@ func TestErrorFormatter(t *testing.T) {
 		nonascii = &wrapped{"café", nil}
 		newline  = &wrapped{"msg with\nnewline",
 			&wrapped{"and another\none", nil}}
-		fallback  = &wrapped{"fallback", xerrors.New("file does not exist")}
+		fallback  = &wrapped{"fallback", os.ErrNotExist}
 		oldAndNew = &wrapped{"new style", formatError("old style")}
 		framed    = &withFrameAndMore{
 			frame: xerrors.Caller(0),
@@ -105,13 +106,6 @@ func TestErrorFormatter(t *testing.T) {
 		want   string
 		regexp bool
 	}{{
-		err: xerrors.New("foo"),
-		fmt: "%+v",
-		want: "foo:" +
-			"\n    golang.org/x/xerrors_test.TestErrorFormatter" +
-			"\n        .+/golang.org/x/xerrors/fmt_test.go:1\\d\\d",
-		regexp: true,
-	}, {
 		err:  simple,
 		fmt:  "%s",
 		want: "simple",
@@ -162,7 +156,7 @@ func TestErrorFormatter(t *testing.T) {
 		fmt: "%+v",
 		want: "something:" +
 			"\n    golang.org/x/xerrors_test.TestErrorFormatter" +
-			"\n        .+/fmt_test.go:9\\d" +
+			"\n        .+/fmt_test.go:97" +
 			"\n    something more",
 		regexp: true,
 	}, {
@@ -179,10 +173,7 @@ func TestErrorFormatter(t *testing.T) {
 		// Note: no colon after the last error, as there are no details.
 		want: "fallback:" +
 			"\n    somefile.go:123" +
-			"\n  - file does not exist:" +
-			"\n    golang.org/x/xerrors_test.TestErrorFormatter" +
-			"\n        .+/golang.org/x/xerrors/fmt_test.go:9\\d",
-		regexp: true,
+			"\n  - file does not exist",
 	}, {
 		err:  opaque,
 		fmt:  "%s",
@@ -290,12 +281,12 @@ func TestErrorFormatter(t *testing.T) {
 		err:  simple,
 		fmt:  "%T",
 		want: "*xerrors_test.wrapped",
-		// }, {
-		// 	// The behavior for this case is different between go1.12 and go1.13.
-		// 	err:  simple,
-		// 	fmt:  "%🤪",
-		// 	want: "%!🤪(*xerrors_test.wrapped=&{simple <nil>})", // go1.12
-		// 	want: "&{%!🤪(string=simple) <nil>}",                // go1.13
+	}, {
+		err:  simple,
+		fmt:  "%🤪",
+		want: "%!🤪(*xerrors_test.wrapped)",
+		// For 1.13:
+		//  want: "%!🤪(*xerrors_test.wrapped=&{simple <nil>})",
 	}, {
 		err:  formatError("use fmt.Formatter"),
 		fmt:  "%#v",
